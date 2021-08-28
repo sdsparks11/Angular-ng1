21e0af63beb957884298bc5351a0235d17b1132d
sdsparks11 <suberthi1998@gmail.com>
2021-08-28 22:05:54 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-vpsmam
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-vpsmam)
---
+++ src/app/app.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { HelloComponent } from './hello.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { ProductListComponent } from './product-list/product-list.component';
+
+@NgModule({
+  imports:      [ BrowserModule, FormsModule ],
+  declarations: [ AppComponent, HelloComponent, TopBarComponent, ProductListComponent ],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
